629726cca809758c6a38d4539a10dbcc1c91bf34
Rickard Östman <rost@spotify.com>
2015-12-08 08:36:41 +0100

    restructured and introduced a module for helper functions

--- gallery.py
+++ gallery.py
@@ -1,20 +1,23 @@
-# all the imports
 from flask import Flask, request, g, redirect, url_for, abort, render_template, flash, send_file
+from helpers import *
 import os, re
-import getpicbykey
-import getpicsbydate
-import getrandompic
-import getalldateswithpictures
 
-
-# configuration
-DEBUG = True
-
-# create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.debug = True
 
-basedir = "/Users/rickard/Pictures"
+#configuration
+DEBUG = True
+DATABASE = "/tmp/gallery.db"
+
+@app.before_request
+def before_request():
+    g.db = sqlite3.connect(DATABASE)
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
 
 def get_files_and_subdirs_from(workingdir):
     if os.path.isdir(workingdir) & (os.path.abspath(workingdir) == workingdir):
@@ -28,12 +31,7 @@ def get_files_and_subdirs_from(workingdir):
                 files.append(item)
         return files, dirs
     else:
-        return [os.path.abspath(workingdir)], [workingdir]
-
-#@app.route('/')
-#def show_pictures():
-#    pictures, dirs = get_files_and_subdirs_from(basedir)
-#    return render_template('index.html', pictures=pictures, dirs=dirs)
+        return [os.path.abspath(workingdir)], [workingdir]  
 
 @app.route('/select', methods=['GET'])
 def select_album():
@@ -49,30 +47,28 @@ def select_album():
 @app.route('/get_image', methods=['GET'])
 def get_image():
     if request.args.has_key('key'):
-        print "hej"
         key = request.args.get('key')
-        fullpath = getpicbykey.main(key)[1]
+        fullpath = get_pic_by_key(key)[1]
         return send_file(fullpath, mimetype='image/jpeg')
     elif request.args.has_key('random'):
-        print "da"
-        fullpath = getrandompic.main()[1]
-        print fullpath
+        fullpath = get_random()[1]
         return send_file(fullpath, mimetype='image/jpeg')
     else:
-        return send_file("/Users/rickard/Documents/repos/gallery/gtfo.jpg", mimetype='image/jpeg')
+        return send_file("static/gtfo.jpg", mimetype='image/jpeg')
 
 @app.route('/', methods=['GET'])
 def get_calendar_view():
     if request.args:
-        return render_template('calendar.html', dates=getalldateswithpictures.main(str(int(request.args.get('year'))+0)))
+        return render_template('calendar.html', dates=get_calendar(str(int(request.args.get('year'))+0)))
     else:
-        return render_template('calendar.html', dates=getalldateswithpictures.main(2015))
+        return render_template('calendar.html', dates=get_calendar(2015))
 
 @app.route('/get_date', methods=['GET'])
 def get_date():
-    pickeys = getpicsbydate.main(request.args['year'],request.args.get('month'),request.args.get('day'))
+    #pickeys = get_pics_by_date(request.args['year'],request.args.get('month'),request.args.get('day'))
+    pickeys = get_pics_by_timestamp(request.args['year'],request.args.get('month'),request.args.get('day'))
     
     return render_template('calendar-dateview.html', year=request.args.get('year'), month=request.args.get('month'), day=request.args.get('day'), keys=pickeys)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
